c45ca7382635461f45fa5f304b918195861e6426
Georgi N. Georgiev <gngeorgiev.it@gmail.com>
2025-02-20 00:39:31 +0200

    Support hide-cli pipe to simplify yazi calls
    SESSION_ID=1740004660463 hide-cli pipe edit_file "path=$(pwd)/.config"

--- cli/main.rs
+++ cli/main.rs
@@ -13,11 +13,10 @@ fn main() -> Result<()> {
 
     let _session_id = env::var("SESSION_ID")?;
     let command = &args[1];
+    let command_args = &args[2..];
     match command.as_str() {
-        "run" => {
-            let command_args = &args[2..];
-            run_command(&command_args)?;
-        }
+        "run" => run_command(&command_args)?,
+        "pipe" => pipe_command(&command_args)?,
         _ => return Err("invalid command: {command}".into()),
     }
 
@@ -31,6 +30,42 @@ fn run_command(args: &[String]) -> Result<()> {
     cmd.current_dir(env::current_dir()?)
         .envs(env::vars())
         .args(command_args)
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit());
+
+    let mut child = cmd.spawn()?;
+    let status = child.wait()?;
+
+    if !status.success() {
+        std::process::exit(status.code().unwrap_or(1));
+    }
+
+    Ok(())
+}
+
+fn pipe_command(args: &[String]) -> Result<()> {
+    let mut message = String::new();
+    message.push('0'); // protocol version
+    message.push_str(&args[0]); // message type, e.g. edit_file
+    message.push(';');
+    for kv in &args[1..] {
+        // message args, e.g. path=/tmp
+        let mut split = kv.split('=');
+        let k = split.next().ok_or("key: keyvalue pair required")?;
+        let v = split.next().ok_or("value: keyvalue pair required")?;
+
+        message.push_str(&format!("{}={};", k, v));
+    }
+
+    let mut cmd = Command::new("zellij");
+    cmd.arg("action")
+        .arg("pipe")
+        .arg("--plugin")
+        .arg("hide")
+        .arg("--")
+        .arg(message)
+        .envs(env::vars())
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit());
